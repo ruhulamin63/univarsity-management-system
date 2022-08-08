e5075c493d73219663d891ae06a0149dda04ef28
Ruhul Amin <raridoy4@gmail.com>
2022-08-08 15:57:23 +0600

    add db file

--- models/ums.sql
+++ models/ums.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Aug 04, 2022 at 07:03 PM
+-- Generation Time: Aug 08, 2022 at 11:56 AM
 -- Server version: 10.4.24-MariaDB
 -- PHP Version: 7.4.29
 
@@ -24,16 +24,242 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `address`
+--
+
+CREATE TABLE `address` (
+  `id` int(30) NOT NULL,
+  `line_1` varchar(255) NOT NULL,
+  `state_id` int(30) NOT NULL,
+  `zip_code` int(30) NOT NULL,
+  `status` tinyint(1) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `attendance`
+--
+
+CREATE TABLE `attendance` (
+  `id` int(30) NOT NULL,
+  `date` date NOT NULL,
+  `student_id` int(30) NOT NULL,
+  `status` tinyint(1) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `attendance`
+--
+
+INSERT INTO `attendance` (`id`, `date`, `student_id`, `status`) VALUES
+(1, '2022-08-02', 4, 1),
+(2, '2022-08-10', 4, 1),
+(3, '2022-08-09', 4, 0);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `classrooms`
+--
+
+CREATE TABLE `classrooms` (
+  `id` int(30) NOT NULL,
+  `student_id` int(30) NOT NULL,
+  `year` int(30) NOT NULL,
+  `grade_id` int(30) NOT NULL,
+  `section` varchar(30) NOT NULL,
+  `status` int(30) NOT NULL,
+  `remarks` int(30) DEFAULT NULL,
+  `teacher_id` int(30) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `classrooms`
+--
+
+INSERT INTO `classrooms` (`id`, `student_id`, `year`, `grade_id`, `section`, `status`, `remarks`, `teacher_id`) VALUES
+(1, 4, 1, 1, 'C', 1, NULL, 1);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `classroom_students`
+--
+
+CREATE TABLE `classroom_students` (
+  `id` int(30) NOT NULL,
+  `student_id` int(30) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `courses`
+--
+
+CREATE TABLE `courses` (
+  `id` int(30) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `desc` varchar(255) NOT NULL,
+  `grade_id` int(30) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `employee`
+--
+
+CREATE TABLE `employee` (
+  `id` int(30) NOT NULL,
+  `full_name` varchar(255) NOT NULL,
+  `status` tinyint(1) NOT NULL,
+  `address_id` int(20) NOT NULL,
+  `last_changed` date NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `employee`
+--
+
+INSERT INTO `employee` (`id`, `full_name`, `status`, `address_id`, `last_changed`) VALUES
+(1, 'Test', 1, 1, '2022-08-02');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `exams`
+--
+
+CREATE TABLE `exams` (
+  `id` int(30) NOT NULL,
+  `exam_type_id` int(30) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `start_date` date NOT NULL,
+  `status` tinyint(1) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `exams`
+--
+
+INSERT INTO `exams` (`id`, `exam_type_id`, `name`, `start_date`, `status`) VALUES
+(1, 1, 'Algorithm', '2022-08-02', 1);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `exam_results`
+--
+
+CREATE TABLE `exam_results` (
+  `id` int(30) NOT NULL,
+  `student_id` int(30) NOT NULL,
+  `course_id` int(30) NOT NULL,
+  `marks` varchar(30) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `exam_type`
+--
+
+CREATE TABLE `exam_type` (
+  `id` int(30) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `desc` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `exam_type`
+--
+
+INSERT INTO `exam_type` (`id`, `name`, `desc`) VALUES
+(1, 'Algorithm', 'Mid Term');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `grades`
+--
+
+CREATE TABLE `grades` (
+  `id` int(30) NOT NULL,
+  `student_id` int(50) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `marks` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `grades`
+--
+
+INSERT INTO `grades` (`id`, `student_id`, `name`, `marks`) VALUES
+(1, 4, 'Web Technology', '80');
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `notices`
 --
 
 CREATE TABLE `notices` (
   `id` int(50) NOT NULL,
+  `teacher_id` int(30) NOT NULL,
   `notice_title` varchar(255) NOT NULL,
   `notice_desc` longtext NOT NULL,
   `time` time NOT NULL,
   `date` date NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `notices`
+--
+
+INSERT INTO `notices` (`id`, `teacher_id`, `notice_title`, `notice_desc`, `time`, `date`) VALUES
+(1, 1, 'Class cancle', 'Some technical problem !', '22:12:46', '2022-08-10');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `pay_rates`
+--
+
+CREATE TABLE `pay_rates` (
+  `id` int(30) NOT NULL,
+  `emp_id` int(30) NOT NULL,
+  `hourly_rate` varchar(30) NOT NULL,
+  `salary` varchar(30) NOT NULL,
+  `effective_date` date NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `pay_rates`
+--
+
+INSERT INTO `pay_rates` (`id`, `emp_id`, `hourly_rate`, `salary`, `effective_date`) VALUES
+(1, 1, '75', '15000', '2022-08-14');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `states`
+--
+
+CREATE TABLE `states` (
+  `id` int(30) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `abbreviation` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `states`
+--
+
+INSERT INTO `states` (`id`, `name`, `abbreviation`) VALUES
+(1, 'Test', 'Gate man');
 
 -- --------------------------------------------------------
 
@@ -58,8 +284,50 @@ CREATE TABLE `students` (
 --
 
 INSERT INTO `students` (`id`, `user_id`, `student_name`, `student_phone`, `student_program`, `student_dob`, `student_cgpa`, `student_grade`, `student_passing_year`) VALUES
-(1, 67, 'Test8', '2147483647', 'BBA', '2022-08-23', '', '', '0000-00-00'),
-(2, 68, 'Wrrwerewr', '1749549843', 'Bsc in ENG', '2022-08-15', '', '', '0000-00-00');
+(4, 70, 'Nayeem Islam', '2147483647', 'Bsc in CSE', '2022-08-17', '', '', '0000-00-00');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `teachers`
+--
+
+CREATE TABLE `teachers` (
+  `id` int(30) NOT NULL,
+  `full_name` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `phone` varchar(255) NOT NULL,
+  `dob` date NOT NULL,
+  `status` tinyint(1) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `teachers`
+--
+
+INSERT INTO `teachers` (`id`, `full_name`, `email`, `phone`, `dob`, `status`) VALUES
+(1, 'Korim Hossian', 'korim@gmail.com', '11111111111', '2022-08-02', 1);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `times`
+--
+
+CREATE TABLE `times` (
+  `id` int(30) NOT NULL,
+  `emp_id` int(30) NOT NULL,
+  `start_date` date NOT NULL,
+  `end_date` date NOT NULL,
+  `hours_worked` varchar(30) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `times`
+--
+
+INSERT INTO `times` (`id`, `emp_id`, `start_date`, `end_date`, `hours_worked`) VALUES
+(1, 1, '2022-08-03', '2022-09-03', 'yes');
 
 -- --------------------------------------------------------
 
@@ -85,34 +353,103 @@ CREATE TABLE `users` (
 --
 
 INSERT INTO `users` (`id`, `username`, `full_name`, `password`, `email`, `phone`, `program`, `dob`, `user_type`, `image`) VALUES
-(1, 'raridoy', 'Ruhul Amin', '11111111', 'raridoy4@gmail.com', 1743369163, 'CSE', '2021-04-15', 'staff', '../../asset/upload/staff-photostax-verification-system.png'),
-(4, 'raridoy_aiub', 'Rony', 'HellpRony', 'kami05518@gamil.com', 1689385783, 'EEE', '2021-04-15', '', ''),
-(47, 'raridoy.420', 'Ruhul Amin', 'ruhulamin', 'raridoy101@gmail.com', 1743369163, 'IPE', '2021-04-27', '', ''),
-(48, 'raridoy_zero', 'Ruhul Amin', 'needhelp', 'insanruhul@gmail.com', 1743369163, 'ENG', '2021-04-13', '', ''),
-(52, 'utsho', 'Ruhul Amin', '11111111', 'abd@gmail.com', 1689385783, 'B.Sc in ENG', '2022-07-26', '', ''),
-(53, '3535', 'Ruhul Amin', '11111111', 'abd@gmail.com', 1689385783, 'B.Sc in ENG', '2022-07-26', '', ''),
-(54, '35354545', 'Ruhul Amin', '11111111', 'abd@gmail.com', 1689385783, 'B.Sc in ENG', '2022-07-26', '', ''),
-(55, '353545454', 'Ruhul Amin', '11111111', 'abd@gmail.com', 1689385783, 'B.Sc in ENG', '2022-07-26', '', ''),
-(57, 'test', 'Test', '22222222', 'test@gmail.com', 2147483647, 'BBA', '2022-08-04', 'student', '../../asset/upload/fsdfds.jpg'),
-(59, 'test1', 'Test1', '11111111', '', 0, '', '', 'student', ''),
-(60, 'test2', 'Test2', '22222222', '', 0, '', '', 'student', ''),
-(61, 'test3', '', '11111111', '', 0, '', '', 'student', ''),
-(62, 'test4', '', '11111111', '', 0, '', '', 'student', ''),
-(63, '', '', '', '', 0, '', '', 'student', ''),
-(64, 'test5', '', '11111111', '', 0, '', '', 'student', ''),
-(65, 'test6', '', '11111111', '', 0, '', '', 'student', ''),
-(66, 'test7', 'Test7', '11111111', 'test7@gmail.com', 2147483647, 'Bsc in EEE', '2022-08-09', 'student', ''),
-(67, 'test8', 'Test8', '11111111', 'test8@gmail.com', 2147483647, 'BBA', '2022-08-23', 'student', ''),
-(68, 'test9', 'Wrrwerewr t', '22222222', 'test9@gmail.com', 1749549843, 'Bsc in ENG', '2022-08-15', 'student', '../../asset/upload/ll.jpg');
+(70, 'nayeem1', 'Nayeem Islam', '11111111', 'nayeem@gmail.com', 2147483647, 'Bsc in CSE', '2022-08-17', 'student', ''),
+(71, 'test', 'Test', '11111111', 'test@gmail.com', 11111111, '', '2022-08-02', 'staff', NULL);
 
 --
 -- Indexes for dumped tables
 --
 
 --
+-- Indexes for table `address`
+--
+ALTER TABLE `address`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `state_id` (`state_id`);
+
+--
+-- Indexes for table `attendance`
+--
+ALTER TABLE `attendance`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `student_id` (`student_id`);
+
+--
+-- Indexes for table `classrooms`
+--
+ALTER TABLE `classrooms`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `grade_id` (`grade_id`),
+  ADD KEY `teacher_id` (`teacher_id`),
+  ADD KEY `student_id` (`student_id`);
+
+--
+-- Indexes for table `classroom_students`
+--
+ALTER TABLE `classroom_students`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `student_id` (`student_id`);
+
+--
+-- Indexes for table `courses`
+--
+ALTER TABLE `courses`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `grade_id` (`grade_id`);
+
+--
+-- Indexes for table `employee`
+--
+ALTER TABLE `employee`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `address_id` (`address_id`);
+
+--
+-- Indexes for table `exams`
+--
+ALTER TABLE `exams`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `exam_type_id` (`exam_type_id`);
+
+--
+-- Indexes for table `exam_results`
+--
+ALTER TABLE `exam_results`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `student_id` (`student_id`),
+  ADD KEY `course_id` (`course_id`);
+
+--
+-- Indexes for table `exam_type`
+--
+ALTER TABLE `exam_type`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `grades`
+--
+ALTER TABLE `grades`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `student_grade_id` (`student_id`);
+
+--
 -- Indexes for table `notices`
 --
 ALTER TABLE `notices`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `teacher_id` (`teacher_id`);
+
+--
+-- Indexes for table `pay_rates`
+--
+ALTER TABLE `pay_rates`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `emp_id` (`emp_id`);
+
+--
+-- Indexes for table `states`
+--
+ALTER TABLE `states`
   ADD PRIMARY KEY (`id`);
 
 --
@@ -121,6 +458,19 @@ ALTER TABLE `notices`
 ALTER TABLE `students`
   ADD PRIMARY KEY (`id`),
   ADD KEY `user_id` (`user_id`);
+
+--
+-- Indexes for table `teachers`
+--
+ALTER TABLE `teachers`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `times`
+--
+ALTER TABLE `times`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `emp_id` (`emp_id`);
 
 --
 -- Indexes for table `users`
@@ -133,32 +483,185 @@ ALTER TABLE `users`
 --
 
 --
+-- AUTO_INCREMENT for table `address`
+--
+ALTER TABLE `address`
+  MODIFY `id` int(30) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `attendance`
+--
+ALTER TABLE `attendance`
+  MODIFY `id` int(30) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT for table `classrooms`
+--
+ALTER TABLE `classrooms`
+  MODIFY `id` int(30) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT for table `classroom_students`
+--
+ALTER TABLE `classroom_students`
+  MODIFY `id` int(30) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `courses`
+--
+ALTER TABLE `courses`
+  MODIFY `id` int(30) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `employee`
+--
+ALTER TABLE `employee`
+  MODIFY `id` int(30) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `exams`
+--
+ALTER TABLE `exams`
+  MODIFY `id` int(30) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `exam_results`
+--
+ALTER TABLE `exam_results`
+  MODIFY `id` int(30) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `exam_type`
+--
+ALTER TABLE `exam_type`
+  MODIFY `id` int(30) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `grades`
+--
+ALTER TABLE `grades`
+  MODIFY `id` int(30) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
 -- AUTO_INCREMENT for table `notices`
 --
 ALTER TABLE `notices`
-  MODIFY `id` int(50) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(50) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `pay_rates`
+--
+ALTER TABLE `pay_rates`
+  MODIFY `id` int(30) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `states`
+--
+ALTER TABLE `states`
+  MODIFY `id` int(30) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- AUTO_INCREMENT for table `students`
 --
 ALTER TABLE `students`
-  MODIFY `id` int(50) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id` int(50) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT for table `teachers`
+--
+ALTER TABLE `teachers`
+  MODIFY `id` int(30) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT for table `times`
+--
+ALTER TABLE `times`
+  MODIFY `id` int(30) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(5) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=69;
+  MODIFY `id` int(5) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=72;
 
 --
 -- Constraints for dumped tables
 --
 
 --
+-- Constraints for table `address`
+--
+ALTER TABLE `address`
+  ADD CONSTRAINT `address_ibfk_1` FOREIGN KEY (`state_id`) REFERENCES `states` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `attendance`
+--
+ALTER TABLE `attendance`
+  ADD CONSTRAINT `attendance_ibfk_1` FOREIGN KEY (`student_id`) REFERENCES `students` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `classrooms`
+--
+ALTER TABLE `classrooms`
+  ADD CONSTRAINT `classrooms_ibfk_2` FOREIGN KEY (`grade_id`) REFERENCES `grades` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `classrooms_ibfk_3` FOREIGN KEY (`teacher_id`) REFERENCES `teachers` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `classrooms_ibfk_4` FOREIGN KEY (`student_id`) REFERENCES `students` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `classroom_students`
+--
+ALTER TABLE `classroom_students`
+  ADD CONSTRAINT `classroom_students_ibfk_1` FOREIGN KEY (`student_id`) REFERENCES `students` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `courses`
+--
+ALTER TABLE `courses`
+  ADD CONSTRAINT `grade_id` FOREIGN KEY (`grade_id`) REFERENCES `grades` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `exams`
+--
+ALTER TABLE `exams`
+  ADD CONSTRAINT `exams_ibfk_1` FOREIGN KEY (`exam_type_id`) REFERENCES `exam_type` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `exam_results`
+--
+ALTER TABLE `exam_results`
+  ADD CONSTRAINT `exam_results_ibfk_1` FOREIGN KEY (`student_id`) REFERENCES `students` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `exam_results_ibfk_2` FOREIGN KEY (`course_id`) REFERENCES `courses` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `grades`
+--
+ALTER TABLE `grades`
+  ADD CONSTRAINT `grades_ibfk_1` FOREIGN KEY (`student_id`) REFERENCES `students` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `notices`
+--
+ALTER TABLE `notices`
+  ADD CONSTRAINT `notices_ibfk_1` FOREIGN KEY (`teacher_id`) REFERENCES `teachers` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `pay_rates`
+--
+ALTER TABLE `pay_rates`
+  ADD CONSTRAINT `pay_rates_ibfk_1` FOREIGN KEY (`emp_id`) REFERENCES `employee` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
 -- Constraints for table `students`
 --
 ALTER TABLE `students`
   ADD CONSTRAINT `students_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `times`
+--
+ALTER TABLE `times`
+  ADD CONSTRAINT `times_ibfk_1` FOREIGN KEY (`emp_id`) REFERENCES `employee` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
